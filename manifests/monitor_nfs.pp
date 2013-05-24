5c6e3d793abbcbc5edba1918ae7acbf9578d3f2d
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-24 11:02:54 +0900

    add monitor_nfs to monit

---
+++ manifests/monitor_nfs.pp
@@ -0,0 +1,32 @@
+# == Class: monit::monitor_nfs
+#
+# This module configures a nfs mount point to be monitored by Monit
+#
+# === Parameters
+#
+# === Examples
+#
+#  monit::monitor_nfs { 'monit-nfs-imagesp':
+#    mountpoint => '/deploy/shared/public/mobile_banner_ads',
+#    check_file => 'mouter.check',
+#  }
+#
+# === Authors
+#
+# Mingxiang Zhang <ming.zhang@gree.net>
+#
+#
+define monit::monitor_nfs (
+  $ensure        = present,
+  $mountpoint,
+  $check_file,
+) {
+  include monit::params
+
+  file { "${monit::params::conf_dir}/$name.conf":
+    ensure  => $ensure,
+    content => template('monit/monitor_nfs.conf.erb'),
+    notify  => Service[$monit::params::monit_service],
+    require => Package[$monit::params::monit_package],
+  }
+}
